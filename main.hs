9a72bc731076c452cc4fce826e393d258cf70a32
not2ho <4294967295@snu.ac.kr>
2025-07-10 11:02:29 +0900

    addd merger

--- main.hs
+++ main.hs
@@ -1,10 +1,12 @@
 import Data.Set ( Set, fromList )
 import System.FilePath
 import System.Directory (listDirectory, getDirectoryContents, getCurrentDirectory)
+import Data.List (sort, groupBy)
+import GHC.List (foldl1')
 
 type Graph a = [Node a]
 data Node a = NODE a [a]
-    deriving (Show, Functor, Foldable, Traversable, Eq)
+    deriving (Show, Functor, Foldable, Traversable, Eq, Ord)
 
 pmap :: (a -> b) -> [a] -> [b]
 pmap = Prelude.map
@@ -15,7 +17,7 @@ main = do
     files <- listDirectory (currentDir </> "recipes")
     let dirs = map ( (currentDir </>).("recipes" </>) ) files
     recipes <- mapM io dirs
-    let recp = map posetizer $ lengthFilter 2 recipes
+    let recp = link recipes
     let roots = findRoots recp
     print $ (map . map) nodeName (test roots)
 
@@ -51,6 +53,9 @@ posetizer :: [String] -> Node String
 posetizer l = let (s:ss) = (map name. reverse) $ lengthFilter 10 l
             in NODE s ss
 
+link :: [[String]] -> Graph String
+link l = merge $ map posetizer $ lengthFilter 2 l
+
 ings :: Graph String -> Set String
 ings s = fromList $ concatMap (\(NODE a b) -> b) s
 
@@ -78,8 +83,23 @@ svgxml :: String
 svgxml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n <!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'> \n <svg version=\"1.1\" viewBox=\"0 0 2000 2000\" xml:space=\"preserve\" xmlns=\"http://www.w3.org/2000/svg\"> \n \t <path d=\"\"/> \n </svg>"
 
 test :: ([Node String], Graph String) -> [[Node String]]
-test (ns, g) = ns : test (next (ns, g))
+test (ns, g) = if null ns then [] else ns : test (next (ns, g))
 
+(*=) :: (Eq a) => Node a -> Node a -> Bool
+(*=) (NODE a l) (NODE b l') = a == b
+
+(*+) :: Node a -> Node a -> Node a
+(*+) (NODE a l) (NODE b l') = NODE a (l++l')
+
+(*+=) :: (Eq a) => Node a -> Node a -> Either (Node a) (Node a, Node a)
+(*+=) a b = if a *= b then Left (a *+ b) else Right (a , b)
+
+(-*-) :: (Eq a) => Node a -> Node a -> [Node a]
+(-*-) a b = either (:[]) (\(x, y) -> [x,y]) (a *+= b)
+
+merge :: Graph String -> Graph String
+merge g = let gs =groupBy (*=) {- sort if it needed -} g
+            in map (foldl1 (*+)) gs
 
 nodeName :: Node String -> String
 nodeName (NODE a l) = a
