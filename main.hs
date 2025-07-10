6bafcaf48a5fde0e2662c39cc7dc7feb85f9d519
not2ho <4294967295@snu.ac.kr>
2025-07-10 10:11:21 +0900

    fix parser

--- main.hs
+++ main.hs
@@ -2,9 +2,9 @@ import Data.Set ( Set, fromList )
 import System.FilePath
 import System.Directory (listDirectory, getDirectoryContents, getCurrentDirectory)
 
-type Graph a = [Node a] 
+type Graph a = [Node a]
 data Node a = NODE a [a]
-    deriving (Show, Functor, Foldable, Traversable)
+    deriving (Show, Functor, Foldable, Traversable, Eq)
 
 pmap :: (a -> b) -> [a] -> [b]
 pmap = Prelude.map
@@ -16,9 +16,9 @@ main = do
     let dirs = map ( (currentDir </>).("recipes" </>) ) files
     recipes <- mapM io dirs
     let recp = map posetizer $ lengthFilter 2 recipes
-    let roots = findRoots recp (ings recp)
-    print recp
-    
+    let roots = findRoots recp
+    print $ (map . map) nodeName (test roots)
+
 
 parser :: String -> [String]
 parser s = recipe $ cutter s
@@ -32,6 +32,7 @@ cutter [] = []
 recipe :: [String] -> [String]
 recipe (s:ss)
     | s == "key" = takeWhile (/="pattern") ss ++ dropWhile (/="pattern") ss
+    | s == "ingredients" = takeWhile (/="results") ss ++ dropWhile (/="results") ss
     | s == "id" = head ss : recipe ss
     | otherwise = recipe ss
 recipe [] = []
@@ -53,18 +54,32 @@ posetizer l = let (s:ss) = (map name. reverse) $ lengthFilter 10 l
 ings :: Graph String -> Set String
 ings s = fromList $ concatMap (\(NODE a b) -> b) s
 
-findRoots :: Graph String -> Set String -> [Node String]
-findRoots s ings = filter (\(NODE a b) -> a `notElem` ings) s
+findRoots :: Graph String -> ([Node String], Graph String)
+findRoots s
+    | null s = ([],[])
+    | otherwise = let ing = ings s
+                    in (filter (\(NODE a b) -> a `notElem` ing) s, filter (\(NODE a b) -> a `elem` ing) s)
+
+
+next :: ([Node String], Graph String) -> ([Node String], Graph String)
+next (ns,g) = let l = concatMap (\(NODE a b)-> b) ns
+            in (filter (\(NODE a b) -> a `elem` l) g, filter (\(NODE a b) -> a `notElem` l) g)
 
 name :: String -> String
 name = drop 1 . dropWhile (/= ':')
 
 svg :: IO ()
-svg = do 
+svg = do
     currentDir <- getCurrentDirectory
     let dir = currentDir </> "file.svg"
     writeFile dir svgxml
 
---todo
 svgxml :: String
 svgxml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n <!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'> \n <svg version=\"1.1\" viewBox=\"0 0 2000 2000\" xml:space=\"preserve\" xmlns=\"http://www.w3.org/2000/svg\"> \n \t <path d=\"\"/> \n </svg>"
+
+test :: ([Node String], Graph String) -> [[Node String]]
+test (ns, g) = ns : test (next (ns, g))
+
+
+nodeName :: Node String -> String
+nodeName (NODE a l) = a
