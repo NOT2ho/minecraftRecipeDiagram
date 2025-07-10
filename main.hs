a08ffee3b1abb7d00781a8bf45ce3c1dcdb380ae
not2ho <4294967295@snu.ac.kr>
2025-07-10 21:30:33 +0900

    almost done

--- main.hs
+++ main.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE ParallelListComp #-}
 
-import Data.Set ( Set, fromList, unions )
+import Data.Set ( Set, fromList, unions, union, toList )
 import System.FilePath
 import System.Directory (listDirectory, getDirectoryContents, getCurrentDirectory)
 import Data.List (sort, groupBy)
 import GHC.List (foldl1')
 import Data.Bits
+import Data.Map (Map, fromList, findWithDefault)
+import Numeric (showHex)
 
 type Graph a = [Node a]
 data Node a = NODE a [a]
@@ -13,6 +15,9 @@ data Node a = NODE a [a]
 
 pmap :: (a -> b) -> [a] -> [b]
 pmap = Prelude.map
+
+sFromList :: Ord a => [a] -> Set a
+sFromList = Data.Set.fromList
 
 main :: IO ()
 main = do
@@ -23,7 +28,7 @@ main = do
     let recp = link recipes
     let loop = loops recp
     let roots = findRoots recp
-    svg $ [x y | x <- nodetoSVG 10 `map` recp | y <- randomIntTuples 2 5 1000]
+    svg $ graphtoSVG 50 4 9 recp
 
 
 parser :: String -> [String]
@@ -61,7 +66,7 @@ link :: [[String]] -> Graph String
 link l = merge $ map posetizer $ lengthFilter 2 l
 
 ings :: Ord a => Graph a -> Set a
-ings s = fromList $ concatMap (\(NODE a b) -> b) s
+ings s = sFromList $ concatMap (\(NODE a b) -> b) s
 
 findRoots :: Graph String -> ([Node String], Graph String)
 findRoots s
@@ -84,7 +89,7 @@ svg l  = do
     writeFile dir $ svgxml l
 
 svgxml :: [String] -> String
-svgxml l = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n <!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'> \n <svg version=\"1.1\" viewBox=\"0 0 2000 2000\" xml:space=\"preserve\" xmlns=\"http://www.w3.org/2000/svg\"> \n \t" ++ concat l ++ "\n </svg>"
+svgxml l = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n <!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'> \n <svg version=\"1.1\" viewBox=\"0 0 20000 20000\" xml:space=\"preserve\" xmlns=\"http://www.w3.org/2000/svg\"> \n \t" ++ concat l ++ "\n </svg>"
 
 nexts :: Eq a => ([Node a], Graph a) -> [[Node a]]
 nexts (ns, g) = if null ns then [] else ns : nexts (next (ns, g))
@@ -114,8 +119,8 @@ tl (NODE x l) = l
 nodeName :: Node String -> String
 nodeName (NODE a l) = a
 
-text :: String -> Int ->  String -> Int -> Int -> String -> String
-text font size color x y s = "<text x=\"" ++
+text :: String -> Int ->  String -> String -> Int -> Int  -> String
+text font size color s x y = "<text x=\"" ++
         show x ++ "\" y=\"" ++ show y ++ "\"\n"
         ++ "\t\tfont-family=\"" ++ font ++ "\" font-size=\"" ++ show size ++ " \" fill=\"" ++ color ++ "\" >\n"
         ++ "\t" ++ s
@@ -140,9 +145,33 @@ xorshift32inf seed= map ((/ 4294967295) . fromIntegral) $ iterate xorshift32 see
 randomIntTuples :: Int -> Int -> Float -> [(Int, Int)]
 randomIntTuples r1 r2 s = zip (map (round . (*s)) $ xorshift32inf r1) (map (round . (*s)) $ xorshift32inf r2)
 
-nodetoSVG :: Show a => Int -> Node a -> (Int, Int) -> String
-nodetoSVG i (NODE a l) (r1, r2)  = text "Super Sans" i "blue" r1 r2 (show a)
+-- nodetoSVG :: Show a => Int -> Node a -> (Int, Int) -> String
+-- nodetoSVG i (NODE a l) (r1, r2)  = text "Super Sans" i "blue" r1 r2 (show a)
 
-path :: (Int, Int) -> (Int, Int) -> Int -> String -> String 
-path (x,y) (x', y') stroke color = "<path d=" ++ "\"M " ++ show x ++ " " ++ show y ++ " " ++ "L " ++ show x' ++ " " ++ show y' ++ " "++ "z\"\n"
-       ++  "stroke=\"" ++ color ++ "stroke-width=\"" ++ show stroke ++ "\" />"
+path :: (Int, Int) -> Int -> String -> (Int, Int) -> String
+path (x,y) stroke color (x', y')  = "<path d=" ++ "\"M " ++ show x ++ " " ++ show y ++ " " ++ "L " ++ show x' ++ " " ++ show y' ++ " "++ "z\"\n"
+       ++  "\tstroke=\"" ++ color ++ "\" stroke-width=\"" ++ show stroke ++ "\"/>\n"
+
+coord :: Ord a => Int -> Int -> Graph a -> Map a (Int, Int)
+coord i j g = let all = toList $ sFromList (concatMap tl g) `union` sFromList (map hd g)
+            in Data.Map.fromList [(a , b) | a <- all | b <- randomIntTuples i j 10000]
+
+
+graphtoSVG ::  (Ord a, Show a) => Int ->  Int -> Int -> Graph a -> [String]
+graphtoSVG s i j g = let c = coord i j g
+                    in concatMap (\x -> map
+                       (path (findWithDefault (0, 0) (hd x) c) 2 (colorCode $ fst (findWithDefault (0, 0) (hd x) c))
+                          . flip (findWithDefault (0, 0)) c)
+                       (tl x)) g
+                       ++ map (\x -> text "Super Sans" s "blue" (show $ hd x) `uncurry` findWithDefault (0,0) (hd x) c) g
+
+colorCode :: Int -> String
+colorCode i = "#" ++ concat ((take 6 . drop 2) $ random16 i)
+
+random16 :: Int -> [String]
+random16 i = map (hex . round .  (*15)) (xorshift32inf i)
+
+hex :: Int -> String
+hex i = if i > 9 
+    then (if i == 10 then "A" else if i == 11 then "B" else if i == 12 then "C" else if i == 13 then "D" else if i == 14 then "E" else "F") 
+    else show i
