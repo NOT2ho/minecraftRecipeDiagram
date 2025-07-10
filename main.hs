9fd019164d6db05916d0aee8a2abdd012cd89175
not2ho <4294967295@snu.ac.kr>
2025-07-10 19:47:27 +0900

    ,

--- main.hs
+++ main.hs
@@ -1,8 +1,11 @@
-import Data.Set ( Set, fromList )
+{-# LANGUAGE ParallelListComp #-}
+
+import Data.Set ( Set, fromList, unions )
 import System.FilePath
 import System.Directory (listDirectory, getDirectoryContents, getCurrentDirectory)
 import Data.List (sort, groupBy)
 import GHC.List (foldl1')
+import Data.Bits
 
 type Graph a = [Node a]
 data Node a = NODE a [a]
@@ -18,8 +21,9 @@ main = do
     let dirs = map ( (currentDir </>).("recipes" </>) ) files
     recipes <- mapM io dirs
     let recp = link recipes
+    let loop = loops recp
     let roots = findRoots recp
-    print $ (map . map) nodeName (test roots)
+    svg $ [x y | x <- nodetoSVG 10 `map` recp | y <- randomIntTuples 2 5 1000]
 
 
 parser :: String -> [String]
@@ -56,7 +60,7 @@ posetizer l = let (s:ss) = (map name. reverse) $ lengthFilter 10 l
 link :: [[String]] -> Graph String
 link l = merge $ map posetizer $ lengthFilter 2 l
 
-ings :: Graph String -> Set String
+ings :: Ord a => Graph a -> Set a
 ings s = fromList $ concatMap (\(NODE a b) -> b) s
 
 findRoots :: Graph String -> ([Node String], Graph String)
@@ -66,24 +70,24 @@ findRoots s
                     in (filter (\(NODE a b) -> a `notElem` ing) s, filter (\(NODE a b) -> a `elem` ing) s)
 
 
-next :: ([Node String], Graph String) -> ([Node String], Graph String)
+next :: Eq a => ([Node a], Graph a) -> ([Node a], Graph a)
 next (ns,g) = let l = concatMap (\(NODE a b)-> b) ns
             in (filter (\(NODE a b) -> a `elem` l) g, filter (\(NODE a b) -> a `notElem` l) g)
 
 name :: String -> String
 name = drop 1 . dropWhile (/= ':')
 
-svg :: IO ()
-svg = do
+svg :: [String] -> IO ()
+svg l  = do
     currentDir <- getCurrentDirectory
     let dir = currentDir </> "file.svg"
-    writeFile dir svgxml
+    writeFile dir $ svgxml l
 
-svgxml :: String
-svgxml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n <!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'> \n <svg version=\"1.1\" viewBox=\"0 0 2000 2000\" xml:space=\"preserve\" xmlns=\"http://www.w3.org/2000/svg\"> \n \t <path d=\"\"/> \n </svg>"
+svgxml :: [String] -> String
+svgxml l = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n <!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'> \n <svg version=\"1.1\" viewBox=\"0 0 2000 2000\" xml:space=\"preserve\" xmlns=\"http://www.w3.org/2000/svg\"> \n \t" ++ concat l ++ "\n </svg>"
 
-test :: ([Node String], Graph String) -> [[Node String]]
-test (ns, g) = if null ns then [] else ns : test (next (ns, g))
+nexts :: Eq a => ([Node a], Graph a) -> [[Node a]]
+nexts (ns, g) = if null ns then [] else ns : nexts (next (ns, g))
 
 (*=) :: (Eq a) => Node a -> Node a -> Bool
 (*=) (NODE a l) (NODE b l') = a == b
@@ -101,5 +105,44 @@ merge :: Graph String -> Graph String
 merge g = let gs =groupBy (*=) {- sort if it needed -} g
             in map (foldl1 (*+)) gs
 
+hd :: Node a -> a
+hd (NODE x l) = x
+
+tl :: Node a -> [a]
+tl (NODE x l) = l
+
 nodeName :: Node String -> String
 nodeName (NODE a l) = a
+
+text :: String -> Int ->  String -> Int -> Int -> String -> String
+text font size color x y s = "<text x=\"" ++
+        show x ++ "\" y=\"" ++ show y ++ "\"\n"
+        ++ "\t\tfont-family=\"" ++ font ++ "\" font-size=\"" ++ show size ++ " \" fill=\"" ++ color ++ "\" >\n"
+        ++ "\t" ++ s
+        ++ "\n</text>\n"
+
+isLoop :: Ord a => Graph a -> Node a -> Bool
+isLoop g n = hd n `elem` concat (foldl1 (++) ((map . map) tl $ nexts ([n], g)))
+
+loops :: Ord a => Graph a -> [Node a]
+loops g = filter (isLoop g) g
+
+xorshift32 :: (Num a, Bits a) => a -> a
+xorshift32 seed =
+    let l13seed = seed .^. (seed .<<. 13) in
+    let r17seed = l13seed .^. (l13seed .>>. 17) in
+    let l5seed = r17seed .^. (r17seed .<<. 5) in
+        l5seed .&. 0xFFFFFFFF
+
+xorshift32inf :: Int -> [Float]
+xorshift32inf seed= map ((/ 4294967295) . fromIntegral) $ iterate xorshift32 seed
+
+randomIntTuples :: Int -> Int -> Float -> [(Int, Int)]
+randomIntTuples r1 r2 s = zip (map (round . (*s)) $ xorshift32inf r1) (map (round . (*s)) $ xorshift32inf r2)
+
+nodetoSVG :: Show a => Int -> Node a -> (Int, Int) -> String
+nodetoSVG i (NODE a l) (r1, r2)  = text "Super Sans" i "blue" r1 r2 (show a)
+
+path :: (Int, Int) -> (Int, Int) -> Int -> String -> String 
+path (x,y) (x', y') stroke color = "<path d=" ++ "\"M " ++ show x ++ " " ++ show y ++ " " ++ "L " ++ show x' ++ " " ++ show y' ++ " "++ "z\"\n"
+       ++  "stroke=\"" ++ color ++ "stroke-width=\"" ++ show stroke ++ "\" />"
